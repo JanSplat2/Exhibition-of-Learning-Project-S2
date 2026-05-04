74032c082892f06d28f8702edb0dbc1dcd8955aa
JanSplat2 <jan.splat@outlook.de>
2026-05-04 14:53:40 -0600

    Initial Commit

---
+++ Exhibition of Learning Project/Exhibition_of_Learning_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exhibition_of_Learning_ProjectApp.swift
+//  Exhibition of Learning Project
+//
+//  Created by Dittrich, Jan - Student on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Exhibition_of_Learning_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Exhibition of Learning ProjectTests/Exhibition_of_Learning_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Exhibition_of_Learning_ProjectTests.swift
+//  Exhibition of Learning ProjectTests
+//
+//  Created by Dittrich, Jan - Student on 5/4/26.
+//
+
+import Testing
+@testable import Exhibition_of_Learning_Project
+
+struct Exhibition_of_Learning_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
